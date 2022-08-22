076d9d510332844e2e944a1ac1accfd3dea93021
Victor <uberman59@gmail.com>
2022-08-22 20:45:51 +0500

    arrrow functions complete

--- practise/chapter1/2.1-2.18/Function.js
+++ practise/chapter1/2.1-2.18/Function.js
@@ -40,3 +40,29 @@ if (n < 1) {
 } else {
   alert(pow(x, n));
 }
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  'Вы согласны?',
+  function () {
+    alert('Вы согласились.');
+  },
+  function () {
+    alert('Вы отменили выполнение.');
+  }
+);
+// Замените код Function Expression стрелочной функцией:
+
+const ask = (question, yes, no) => {
+  if (confirm(question)) yes();
+  else no();
+};
+ask(
+  'Вы согласны?',
+  () => alert('Вы согласились.'),
+  () => alert('Вы отменили выполнение.')
+);
