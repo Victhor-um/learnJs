a999b9e0b92db13a1eda9a29a32acea40ea5363e
Victor <uberman59@gmail.com>
2022-08-22 11:53:19 +0500

    variables complete

---
+++ practise/chapter 1/2.1-2.18/variables.js
@@ -0,0 +1,24 @@
+ //correct names
+ let userName;
+let test123;
+let $ = 66.6;
+let _ = 0.30000003;
+//incorrect names
+
+let 14march;
+let my-space;
+
+//Задачи 
+
+let admin;
+let name;
+name = 'John'
+admin = name;
+alert(admin);
+
+let planetEarth;
+let currentVisitorName;
+
+const BIRTHDAY = '02.01.1994';
+
+const age = someCode(birthday);
