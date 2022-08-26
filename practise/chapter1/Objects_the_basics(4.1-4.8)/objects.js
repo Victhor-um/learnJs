c195c0f31b42e5c5603b1de2af34b304e761d42b
Victor <uberman59@gmail.com>
2022-08-26 13:22:36 +0500

    complete objects

--- practise/chapter1/Objects_the_basics(4.1-4.8)/objects.js
+++ practise/chapter1/Objects_the_basics(4.1-4.8)/objects.js
@@ -22,3 +22,17 @@ function sumSalary(obj) {
   for (let props in obj) sum += obj[props];
   return sum;
 }
+
+let menu = {
+  width: 200,
+  height: 300,
+  title: 'My menu',
+};
+
+function multiplyNumeric(menu) {
+  for (let key in menu) {
+    if (typeof menu[key] === 'number') {
+      menu[key] *= 2;
+    }
+  }
+}
