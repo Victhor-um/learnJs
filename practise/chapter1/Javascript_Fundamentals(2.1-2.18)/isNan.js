0472d667df898dd0727f08030fae5238bc604292
Victor <uberman59@gmail.com>
2022-08-29 20:57:48 +0500

    update isNan

--- practise/chapter1/Javascript_Fundamentals(2.1-2.18)/isNan.js
+++ practise/chapter1/Javascript_Fundamentals(2.1-2.18)/isNan.js
@@ -7,6 +7,10 @@ console.log('null', isNaN(null)); // false
 
 console.log('undefined', isNaN(undefined)); // true
 
+console.log(isNaN('37')); // false: "37" is converted to the number 37 which is not NaN
+console.log(isNaN('37.37')); // false: "37.37" is converted to the number 37.37 which is not NaN
+console.log(isNaN('37,5')); // true
+console.log(isNaN('123ABC')); // true:  parseInt("123ABC") is 123 but Number("123ABC") is NaN
 console.log('string', isNaN('string')); // true
 
 console.log('Infinity', isNaN(Infinity)); // false
@@ -16,6 +20,11 @@ console.log('[]]', isNaN([])); // false
 console.log('{}', isNaN({})); // true
 
 console.log('5', Number.isNaN(5)); // false
+
+console.log(Number.isNaN('37')); // false: "37" is converted to the number 37 which is not NaN
+console.log(Number.isNaN('37.37')); // false: "37.37" is converted to the number 37.37 which is not NaN
+console.log(Number.isNaN('37,5')); // false
+console.log(Number.isNaN('123ABC')); //false
 
 console.log('NaN', Number.isNaN(NaN)); // true
 
