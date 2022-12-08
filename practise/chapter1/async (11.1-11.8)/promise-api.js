8f5715f225be53423610ffdcdd6df5c180a5d1d8
Victor <uberman59@gmail.com>
2022-12-08 11:48:55 +0500

    complete promise-api

--- practise/chapter1/async (11.1-11.8)/promise-api.js
+++ practise/chapter1/async (11.1-11.8)/promise-api.js
@@ -1,3 +1,4 @@
+// Promise.all(promises) - ожидает выполнения всех промисов и возвращает массив с результатами. Если любой из  указанных промисов вернет ошибку, то результатом работы будет эта ошибка, результаты остальных промисов будут игнорироваться
 let promise = Promise.all([
   new Promise((resolve) =>
     setTimeout(() => {
@@ -15,7 +16,7 @@ let promise = Promise.all([
     }, 1000)
   ),
 ]).then(console.log(33333));
-
+// Promise.allSettled(promises) - ждет, пока все промисы завершатся и возвращает их результаты в виде массива с объектами, где у каждого объекта два свойства: status : 'fulfilled'/'rejected' успешно выполнен или ошибка. value - результат, если успешно или reason - ошибка, если нет
 let setled = Promise.allSettled([
   new Promise((resolve) =>
     setTimeout(() => {
@@ -43,7 +44,7 @@ setTimeout(() => {
   console.log('promiseALL: ', promise);
   console.log('setled: ', setled);
 }, 8000);
-
+// Promise.race(promises) - ожидает первый выполненый промис, который становится его результатом, остальные игнорируются
 Promise.race([
   new Promise((resolve, reject) => setTimeout(() => resolve(1), 1000)),
   new Promise((resolve, reject) =>
@@ -51,3 +52,42 @@ Promise.race([
   ),
   new Promise((resolve, reject) => setTimeout(() => resolve(3), 3000)),
 ]).then(alert); // 1
+
+// Promise.any(promises) – ожидает первый успешно выполненный промис, который становится его результатом, остальные игнорируются. Если все переданные промисы отклонены, AggregateError становится ошибкой Promise.any
+
+//Результатом будет 1:
+Promise.any([
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      reject(new Error('ГЛАВНАЯ ОШИБКА ТВОЕЙ ЖИЗНИ'));
+    }, 1000)
+  ),
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      resolve(1);
+    }, 2000)
+  ),
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      resolve(2);
+    }, 3000)
+  ),
+]).then(alert);
+
+//Пример где все промисы отклоняются:
+Promise.any([
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      reject('Vot eto pzdc');
+    }, 1500)
+  ),
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      reject('Vot eto AGAIN  pzdc');
+    }, 4500)
+  ),
+]).catch((error) => {
+  console.log(error.constructor.name);
+  console.log(error.errors[0]);
+  console.log(error.errors[1]);
+});
