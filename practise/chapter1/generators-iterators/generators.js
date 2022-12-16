95d38d971130f9774f894cc539dc44e956970a11
Victor <uberman59@gmail.com>
2022-12-16 20:50:39 +0500

    generators

--- practise/chapter1/generators-iterators/generators.js
+++ practise/chapter1/generators-iterators/generators.js
@@ -15,3 +15,27 @@ function* generateSequence3() {
   return 3;
 }
 // Можно по разному, но первый вариант выглядит понятней\удобней ИМХО
+
+let sequence = [0, ...generateSequence()];
+
+console.log('sequence: ', sequence);
+
+/**
+ * Задачей является создать функцию-генератор pseudoRandom(seed), которая получает seed и создаёт генератор с указанной формулой.
+
+Пример использования:
+ */
+
+function* pseudoRandom(seed) {
+  let value = seed;
+  while (true) {
+    value = (value * 16807) % 2147483647;
+    yield value;
+  }
+}
+
+let generator = pseudoRandom(1);
+
+console.log(generator.next().value); // 16807
+console.log(generator.next().value); // 282475249
+console.log(generator.next().value); // 1622650073
