660ec1d590aa6b7b354a1ae21af115ab75f04ff0
Victor <uberman59@gmail.com>
2022-12-16 19:50:32 +0500

    microtask-complete

--- practise/chapter1/async (11.1-11.8)/microtask-queue.js
+++ practise/chapter1/async (11.1-11.8)/microtask-queue.js
@@ -1,1 +1,21 @@
 //Обработчики промисов .then/.catch/.finally всегда асинхронны.
+
+let promise = Promise.resolve();
+
+promise.then(() => console.log(2));
+
+console.log(1);
+
+// будет 1 потом 2
+
+let reject = Promise.reject(new Error('Eba tyt oshibka'));
+
+setTimeout(() => {
+  reject.catch((err) => console.log('tipo poimal'));
+}, 1500);
+
+window.addEventListener('unhandledrejection', (event) =>
+  console.log(event.reason)
+);
+//unhandledrejection срабатывает ибо очередь микрозадач завершена, промис в состоянии режектед, и генерит событие
+//catch срабатывает уже после unhandledrejection
