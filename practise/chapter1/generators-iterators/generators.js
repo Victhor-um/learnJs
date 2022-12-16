c5da41de29cb59b9d9e1fcf7fd6f6374923600bb
Victor <uberman59@gmail.com>
2022-12-16 18:13:10 +0500

    microtast-que

---
+++ practise/chapter1/generators-iterators/generators.js
@@ -0,0 +1,17 @@
+function* generateSequence() {
+  yield 1;
+  yield 2;
+  return 3;
+}
+function* generateSequence2() {
+  yield 1;
+  yield 2;
+  return 3;
+}
+
+function* generateSequence3() {
+  yield 1;
+  yield 2;
+  return 3;
+}
+// Можно по разному, но первый вариант выглядит понятней\удобней ИМХО
